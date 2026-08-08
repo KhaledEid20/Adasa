4a8716e2196889b410b4fe9c571fc371de18f3f4
Khaled_Eid <khaled_eid77@outlook.com>
2026-08-09 01:53:13 +0300

    feature/RoutingInitiated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'',component:HomeComponent},
+    {path:'blog',component:BlogComponent},
+    {path:'about',component:AboutComponent}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from "./components/blog/blog.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , NavBarComponent ,HomeComponent],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent, BlogComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
