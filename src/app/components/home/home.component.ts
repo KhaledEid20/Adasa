4ac3b8bf515834d067d8f6153528220eab689a5d
Khaled_Eid <khaled_eid77@outlook.com>
2026-08-15 04:53:56 +0300

    home/top-articles section done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Article} from '../../../assets/Models/Article'
+import articleData from '../../../assets/data/posts.json'
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  articles: Article[] = articleData.posts;
+  get filteredArticles() {
+    return this.articles.filter(a => a.id <= 3);
+}
 }
